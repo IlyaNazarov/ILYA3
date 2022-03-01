f5cf52a78ae043e6dca4b4ab1b4379870c00406d
IlyaNazarov <ilyanazarov1986@gmail.com>
2022-03-02 00:12:50 +0300

    Lesson 3 finish

---
+++ src/lesson3/Fruit.java
@@ -0,0 +1,14 @@
+package lesson3;
+
+public class Fruit {
+
+    private final float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
---
+++ src/lesson3/Main.java
@@ -0,0 +1,58 @@
+package lesson3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main (String[] args) {
+        String[] arr = {"abs", "ds","ret", "123"};
+
+        System.out.println("Task 1\n" + Arrays.toString(arr));
+        swapElements(arr);
+        System.out.println(Arrays.toString(arr));
+
+        List<String> list = convertToList(arr);
+        System.out.println("Task 2\n" + list.getClass() + " : " + list);
+
+        System.out.println("Task 3");
+        Box<Orange> OrangeBox = new Box<>();
+        Box<Apple> AppleBox = new Box<>();
+            OrangeBox.add(new Orange());
+            AppleBox.add(new Apple());
+
+        for (int i = 0; i < 4; i++) {
+            OrangeBox.add(new Orange());
+        }
+            for (int i =0; i < 6; i++) {
+                AppleBox.add(new Apple());
+            }
+
+        OrangeBox.info();
+        AppleBox.info();
+
+        float OrangeWeigth = OrangeBox.getWeight();
+        float AppleWeigth = AppleBox.getWeight();
+        System.out.println("Вес коробки с апельсинами: " + OrangeWeigth);
+        System.out.println("Вес коробки с яблоками: " + AppleWeigth);
+
+        System.out.println("Сравнить вес OrangeBox и AppleBox: " + OrangeBox.compare(AppleBox));
+
+                 OrangeBox.moveAt(OrangeBox);
+
+        OrangeBox.info();
+        AppleBox.info();
+
+    }
+
+    private static <T> void swapElements(T[] array) {
+        T temp = array[2];
+        array[2] = array[3];
+        array[3] = temp;
+    }
+
+    private static <E> List<E> convertToList(E[] array) {
+        return  Arrays.asList(array);
+    }
+
+}
